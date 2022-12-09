4c4a6b5b22c2445f5bf5c49934be39730bdf368a
Bearaujus <haryobagasasyafah6@gmail.com>
2022-12-09 13:57:17 +0700

    Update worker code documentation

--- worker/init.go
+++ worker/init.go
@@ -2,22 +2,26 @@ package worker
 
 import "sync"
 
-// Worker is interface used by worker
+// Worker is interface used by worker.
 type Worker interface {
 	// PushJob is used to push errorless job to the worker.
 	PushJob(job func())
 
-	// PushJobWithError is used to push a job with an error to the worker. if an error occurs, it will send to the listener.
+	// PushJobWithError is used to push a job with an error to the worker. if an error occurs,
+	// it will send to the listener.
+	//
+	// Note: if the listener is nil, the error will be dismissed.
 	PushJobWithError(job func() error)
 
 	// Wait is used to wait for all workers to finish all jobs in the job channel.
 	Wait()
 
-	// GracefulShutdown is used to wait for all workers to finish all jobs in the job channel and gracefully shutdown all workers.
+	// GracefulShutdown is used to wait for all workers to finish all jobs in the job channel
+	// and gracefully shutdown all workers and channels.
 	GracefulShutdown()
 }
 
-// worker is a struct to wrap all the worker data
+// worker is a struct to implement Worker interface.
 type worker struct {
 	numWorker    int
 	jobChannel   chan func() error
@@ -30,8 +34,12 @@ type worker struct {
 
 // NewWorker is used to create a new Worker.
 // numWorker is the number of workers to execute jobs synchronously (default 1).
-// maxRetries is max retries to repeat the job if an error occurs (default 1).
-// errorListenerFunc is a user custom error handling when a job occurs error (default nil).
+// maxRetries is the maximum number of retries to repeat the job if an error occurs (default 1).
+// errorListenerFunc is a user custom error handling when a job occurs the error (default nil).
+//
+// After calling NewWorker, this will immediately execute listenJobChannel X times and listenErrorChannel 1 time,
+// where X is equal to numWorker. So we don't need a function to start the worker,
+// its already started when calling NewWorker.
 //
 // Note: if numWorker/maxRetries < default value, it will automatically adjust to the default value.
 // if errorListenerFunc is nil, it will not listen to the error channel and the error channel will be not activated.
